ac4e283db46dd9b5fc8597624d5d32be6dc60285
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-05-01 15:54:08 +0200

    refactor: use Hash_set.strict_add

--- test/test__piece.ml
+++ test/test__piece.ml
@@ -11,20 +11,19 @@ let%expect_test "indices" =
 ;;
 
 let%expect_test "components" =
-  let colors = Hash_set.create (module Color) in
+  let visited_colors = Hash_set.create (module Color) in
   List.iter Piece.all ~f:(fun piece ->
     let color = Piece.color piece in
-    let () =
-      if Hash_set.mem colors color
-      then raise_s [%sexp "Duplicated color", (color : Color.t)]
-      else Hash_set.add colors color
-    in
+    (match Hash_set.strict_add visited_colors color with
+     | Ok () -> ()
+     | Error e -> raise_s [%sexp "Duplicated color", (color : Color.t), (e : Error.t)]);
     let components = Piece.components piece in
-    let hset = Hash_set.create (module Coordinate) in
+    let visited_components = Hash_set.create (module Coordinate) in
     List.iter components ~f:(fun component ->
-      if Hash_set.mem hset component
-      then raise_s [%sexp "Duplicated component", (component : Coordinate.t)]
-      else Hash_set.add hset component);
+      match Hash_set.strict_add visited_components component with
+      | Ok () -> ()
+      | Error e ->
+        raise_s [%sexp "Duplicated component", (component : Coordinate.t), (e : Error.t)]);
     let length = List.length components in
     assert (4 <= length && length <= 5));
   [%expect {||}];
