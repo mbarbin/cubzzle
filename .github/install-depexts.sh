4c07fdabaf586337b04602822911cef68db9a1d8
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-07-09 22:51:03 +0200

    Less verbose install-depexts

--- .github/install-depexts.sh
+++ .github/install-depexts.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # This script installs system dependencies (depexts) listed by `dune show depexts`.
 # It is intended to be called from the setup-dune composite action.
 
-echo "[install-depexts] Starting."
+echo "::debug::[install-depexts] Starting."
 
 : "${RUNNER_OS:?RUNNER_OS must be set}"
 
@@ -19,21 +19,19 @@ else
 fi
 rm -f "$DEPEXT_TEMPFILE"
 
-echo "[install-depexts] DEPEXT_LIST='$(printf '%s' "$DEPEXT_LIST" | sed ':a;N;$!ba;s/\n/\\n/g')'"
+echo "[install-depexts] DEPEXT_LIST='$(printf '%s' "$DEPEXT_LIST" | awk '{printf "%s\\n", $0}')'"
 
 if [ -z "$DEPEXT_LIST" ]; then
-  echo "[install-depexts] No depexts to install."
+  echo "::debug::[install-depexts] No depexts to install."
   exit 0
 fi
 
 if [ "${RUNNER_OS}" = "Linux" ]; then
-  echo "[install-depexts] Installing depexts with apt-get:"
-  echo "$DEPEXT_LIST"
+  echo "::debug::[install-depexts] Installing depexts with apt-get:"
   sudo apt-get update
   xargs -r sudo apt-get install -y <<< "$DEPEXT_LIST"
 elif [ "${RUNNER_OS}" = "macOS" ]; then
-  echo "[install-depexts] Installing depexts with brew:"
-  echo "$DEPEXT_LIST"
+  echo "::debug::[install-depexts] Installing depexts with brew:"
   brew update
   xargs -r brew install <<< "$DEPEXT_LIST"
 else
