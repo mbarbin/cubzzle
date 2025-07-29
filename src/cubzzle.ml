2f0613ac326e4ddeeccd466e4f3dc6f8497906fa
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-07-29 09:03:29 +0200

    Switch commands to let+ style

--- src/cubzzle.ml
+++ src/cubzzle.ml
@@ -180,14 +180,15 @@ let interactive_view box =
 let run_cmd =
   Command.make
     ~summary:"Run the solver."
-    (let%map_open.Command shape =
+    (let open Command.Std in
+     let+ shape =
        Arg.named_with_default
          [ "shape" ]
          (Param.enumerated (module Z_shape.Sample))
          ~default:Z_shape.Sample.Cube
          ~docv:"SHAPE"
          ~doc:"Specify which shape to solve."
-     and draw_box_during_search =
+     and+ draw_box_during_search =
        Arg.named_with_default
          [ "draw-box-during-search" ]
          Param.bool
